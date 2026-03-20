f1dc7c95f3f2f29c6460f8c3b6ad8bb98721325e
Kabilan0221 <kabilangnanasekeran@gmail.com>
2026-03-20 14:01:15 +0530

    Added Railway deployment config

--- php/db_redis.php
+++ php/db_redis.php
@@ -3,7 +3,8 @@ try {
     // Requires phpredis extension
     $redis = new Redis();
     $redis->connect('127.0.0.1', 6379);
-} catch (Exception $e) {
+}
+catch (Exception $e) {
     http_response_code(500);
     echo json_encode(['error' => 'Redis connection failed: ' . $e->getMessage()]);
     exit;
